fac7b8c47caceee2228d50f0633e1c9729bdc3bc
SebastianSavi <SebastianSavi@users.noreply.github.com>
2016-10-31 09:18:42 +0100

    Ajouté le menu quand on clique sur le bouton bas
    Menu a completer

--- src/c/main.c
+++ src/c/main.c
@@ -27,6 +27,11 @@ Good luck and have fun!
 #define NB_SAMPLE   25
 #define DATA_NUM    3
 
+// Definitions for simmple menu
+#define NB_MENU_SECTIONS 1
+#define NB_SETTINGS_ITEMS 4
+#define NB_DISPLAY 4
+
 // Circular array storing the last NB_SAMPLE acceleromter values
 static int last_data[DATA_NUM][NB_SAMPLE] = {{0}, {0}, {0}};
 static int idx=0;
@@ -38,10 +43,98 @@ static GBitmap *wheel;
 static BitmapLayer *happy_smiley_layer;
 static BitmapLayer *wheel_layer;
 // Declare the main window and two text layers
-Window *main_window;
-TextLayer *background_layer;
-TextLayer *title_layer;
-TextLayer *data_layer;
+Window *main_window, *menu_window;
+TextLayer *background_layer,*title_layer, *data_layer;
+
+// Declare main variable & layers for simple menu
+static SimpleMenuLayer *main_menu_layer;
+static SimpleMenuSection menu_sections[NB_MENU_SECTIONS];
+static SimpleMenuItem settings_items[NB_SETTINGS_ITEMS];
+static bool gender_flag = false;
+static int display_type = 0;
+
+/////// We configure the clicks on the main window//////////////////////////////////////
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {        //
+  // A single click has just occured                                                  //
+    	window_stack_push(menu_window, false);
+
+}
+
+static void click_config_provider(void *context) {
+  // Subcribe to button click events here
+  ButtonId id = BUTTON_ID_DOWN;  // The Select button
+
+  window_single_click_subscribe(id, down_click_handler);                              //
+}                                                                                     //
+////////////////////////////////////////////////////////////////////////////////////////
+
+///////"Menu part"////////////////////////////////////////////////////////////////////
+                                                                                    //
+//Gender callback                                                                   //
+static void gender_select_callback(int index, void *ctx) {                          //
+  gender_flag = !gender_flag;                                                       //
+  
+  SimpleMenuItem *menu_item = &settings_items[index];
+  
+  if (gender_flag) {
+    menu_item->subtitle = "Male";
+  } else {
+    menu_item->subtitle = "Female";
+  }
+  layer_mark_dirty(simple_menu_layer_get_layer(main_menu_layer));
+}
+
+static void display_select_callback(int index, void *ctx) {                          //
+                                                                                     //
+  display_type = (display_type + 1) % NB_DISPLAY;
+  SimpleMenuItem *menu_item = &settings_items[index];
+  
+  if (gender_flag) {
+    menu_item->subtitle = "Male";
+  } else {
+    menu_item->subtitle = "Female";
+  }
+  layer_mark_dirty(simple_menu_layer_get_layer(main_menu_layer));
+}
+
+static void menu_window_load(Window *window) {
+  settings_items[0] = (SimpleMenuItem) {
+    .title = "Hight",
+    .subtitle = "Select your size",
+  };
+  settings_items[1] = (SimpleMenuItem) {
+    .title = "Gender",
+    .subtitle = "Click to choose",
+    .callback = gender_select_callback,
+  };
+
+  settings_items[2] = (SimpleMenuItem) {
+    .title = "Daily goal",
+  };
+   settings_items[3] = (SimpleMenuItem) {
+    .title = "Display",
+    .callback = display_select_callback,
+  };
+
+  menu_sections[0] = (SimpleMenuSection) {
+    .title = "Settings",
+    .num_items = NB_SETTINGS_ITEMS,
+    .items = settings_items,
+  };
+ 
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  main_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NB_MENU_SECTIONS, NULL);
+
+  layer_add_child(window_layer, simple_menu_layer_get_layer(main_menu_layer));
+}
+
+void main_window_unload(Window *window) {
+  simple_menu_layer_destroy(main_menu_layer);
+                                                                                    //
+}                                                                                   //
+//////////////////////////////////////////////////////////////////////////////////////
 
 // Init function called when app is launched
 static void init(void) {
@@ -55,12 +148,14 @@ static void init(void) {
     accel_service_set_sampling_rate(ACCEL_SAMPLING_25HZ);
 }
 
+
+
 static void build_ui() {
     /* == MAIN WINDOW == */
     // Create main Window element and assign to pointer
   	main_window = window_create();
     Layer *window_layer = window_get_root_layer(main_window);  
-
+    window_set_click_config_provider(main_window, click_config_provider);
     /* == BACKGROUND == */
 		// Create background Layer
 		background_layer = text_layer_create(GRect(0, 0, 144, 168));
@@ -105,7 +200,19 @@ static void build_ui() {
 	  layer_add_child(window_layer, text_layer_get_layer(data_layer));
     layer_add_child(window_layer, bitmap_layer_get_layer(happy_smiley_layer));
     layer_add_child(window_layer, bitmap_layer_get_layer(wheel_layer));
-  	// Show the window on the watch, with animated = true
+  	
+  
+    /* == MENU WINDOW == */
+    // Create main Window element and assign to pointer
+  	menu_window = window_create();
+    Layer *menu_window_layer = window_get_root_layer(menu_window);  
+    
+    window_set_window_handlers(menu_window, (WindowHandlers) {
+    .load = menu_window_load,
+    .unload = main_window_unload,
+  });
+  
+    // Show the main window on the watch, with animated = true
   	window_stack_push(main_window, true);
 }
 
@@ -128,7 +235,7 @@ static void accel_data_handler(AccelData *data,uint32_t num_samples)
   
   //Print the results in the LOG
   APP_LOG(APP_LOG_LEVEL_INFO, "x: %d, y: %d, z: %d", x, y, z);
-} 
+ 
   
   //Print the results on the watch
   snprintf(results, 60, "x: %d, y: %d, z: %d", x, y, z);
